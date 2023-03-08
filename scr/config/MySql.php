95e14f5aff6a2876b05e9e0a24bbbcd99374d8b3
Raiane Miranda <raianemirandela@gmail.com>
2023-03-08 17:04:39 -0300

    primeiro commit

---
+++ scr/config/MySql.php
@@ -0,0 +1,14 @@
+<?php
+//Define your host here.
+$HostName = "ftp.web2149.uni5.net";
+ 
+//Define your database name here.
+$DatabaseName = "docks_pi";
+ 
+//Define your database username here.
+$HostUser = "nw203";
+ 
+//Define your database password here.
+$HostPass = "Senac@2024";
+ 
+?>
