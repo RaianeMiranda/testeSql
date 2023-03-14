e4b5d791e69cef52df8a33713864a60348290224
Raiane Miranda <raianemirandela@gmail.com>
2023-03-14 14:38:27 -0300

    adicionando teste de MySql

--- data/MySql.js
+++ data/MySql.js
@@ -1,21 +1,34 @@
+const mysql      = require('mysql2');
+const connection = mysql.createConnection({
+  host     : '34.151.200.120',
+  port     : 3000,
+  user     : 'raiane',
+  password : 'adm1123',
+  database : 'docks'
+});
+
+connection.connect((err) => {
+  if(err) return console.log(err);
+  console.log('conectou!');
+})
 // Sample code with mysql
-var Sequelize = require('sequelize');
-var mysql = require('mysql2/promise');
-var connection = mysql.createConnection({
-    host: '34.151.200.120',
-    user: 'raiane',
-    password: 'adm1123',
-    database: 'docks',
-});
+// var Sequelize = require('sequelize');
+// var mysql = require('mysql2/promise');
+// var connection = mysql.createConnection({
+//     host: '34.151.200.120',
+//     user: 'raiane',
+//     password: 'adm1123',
+//     database: 'docks',
+// });
 
-connection.connect();
+// connection.connect();
 
-connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-  if (error) throw error;
-  console.log('The solution is: ', results[0].solution);
-});
+// connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+//   if (error) throw error;
+//   console.log('The solution is: ', results[0].solution);
+// });
 
-connection.end();
+// connection.end();
 
-export default mysql;
-module.exports = Sequelize;
+// export default mysql;
+// module.exports = Sequelize;
