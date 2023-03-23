24d942cb5dffcd898e562fba11e32934a45f7083
Raiane Miranda <raianemirandela@gmail.com>
2023-03-23 15:45:05 -0300

    revertendo commit

--- client/App.js
+++ client/App.js
@@ -1,11 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import Test from './src/App';
 
 export default function App() {
   return (
-   <div>
-   <Test></Test>
-   </div>
+    <Test />
   );
 }
--- client/src/App.js
+++ client/src/App.js
@@ -1,88 +1,84 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
-import Axios from "axios";
-import Card from "./components/cards/card";
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
+import { View, Text } from 'react-native';
+import { TextInput, Button, Card } from 'react-native-paper';
 
-function App() {
+function Test() {
   const [values, setValues] = useState({});
   const [listUsuario, setListUsuario] = useState([]);
 
-  const handleChangeValues = (event) => {
+  const handleChangeValues = (name, value) => {
     setValues((prevValue) => ({
       ...prevValue,
-      [event.target.name]: event.target.value,
+      [name]: value,
     }));
   };
 
   const handleClickButton = () => {
-    Axios.post("http://localhost:3306/register", {
+    Axios.post('http://localhost:3306/register', {
       idEmail: values.email,
       nome: values.name,
       senha: values.senha,
-    })
-      .then(() => {
-        setListUsuario([
-          ...listUsuario,
-          {
-            idEmail: values.email,
-            nome: values.name,
-            senha: values.senha,
-          }
-        ])
-      });
+    }).then(() => {
+      setListUsuario([
+        ...listUsuario,
+        {
+          idEmail: values.email,
+          nome: values.name,
+          senha: values.senha,
+        },
+      ]);
+    });
   };
 
   useEffect(() => {
-    Axios.get("http://localhost:3306/getCards")
+    Axios.get('http://localhost:3306/getCards')
       .then((response) => {
         setListUsuario(response.data);
       })
       .catch((error) => {
         console.error(error);
       });
-      
   }, []);
 
   return (
-    <div className="app-container">
-      <div className="register-container">
-        <h1>Cadastro</h1>
-        <input
-          type="text"
-          name="name"
+    <View style={{ flex: 1 }}>
+      <View style={{ alignItems: 'center', marginTop: 50 }}>
+        <Text style={{ fontSize: 24, fontWeight: 'bold' }}>Cadastro</Text>
+        <TextInput
           placeholder="nome"
-          className="register-input"
-          onChange={handleChangeValues}
+          style={{ borderWidth: 1, borderRadius: 5, padding: 10, margin: 10, width: 250 }}
+          value={values.name}
+          onChangeText={(value) => handleChangeValues('name', value)}
         />
-        <input
-          type="text"
-          name="email"
+        <TextInput
           placeholder="email"
-          className="register-input"
-          onChange={handleChangeValues}
+          style={{ borderWidth: 1, borderRadius: 5, padding: 10, margin: 10, width: 250 }}
+          value={values.email}
+          onChangeText={(value) => handleChangeValues('email', value)}
         />
-        <input
-          type="password"
-          name="senha"
+        <TextInput
+          secureTextEntry
           placeholder="senha"
-          className="register-input"
-          onChange={handleChangeValues}
+          style={{ borderWidth: 1, borderRadius: 5, padding: 10, margin: 10, width: 250 }}
+          value={values.password}
+          onChangeText={(value) => handleChangeValues('password', value)}
         />
-        <button onClick={handleClickButton}>Cadastrar</button>
-      </div>
-      {console.log(listUsuario)}
-      {listUsuario.map((value) => (
-        <Card
-          key={value.idEmail}
-          idEmail={value.idEmail}
-          nome={value.nome}
-          senha={value.senha}
-          listUsuario={listUsuario}
-          setListUsuario={setListUsuario}
-        />
-      ))}
-    </div>
+        <Button title="Cadastrar" onPress={handleClickButton} />
+      </View>
+      <View style={{ margin: 10 }}>
+        {listUsuario.map((value) => (
+          <Card key={value.idEmail} style={{ marginVertical: 5 }}>
+            <Card.Content>
+              <Text>{value.nome}</Text>
+              <Text>{value.idEmail}</Text>
+              <Text>{value.senha}</Text>
+            </Card.Content>
+          </Card>
+        ))}
+      </View>
+    </View>
   );
 }
 
-export default App;
+export default Test;
