82d1c1c99c78e16025774d453b50398d9b97a57c
Raiane Miranda <raianemirandela@gmail.com>
2023-03-23 14:07:39 -0300

    abrindo aplicação com expo

---
+++ client/App.js
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Test from './src/App';
+
+export default function App() {
+  return (
+   <div>
+   <Test></Test>
+   </div>
+  );
+}
